b42a0254494417e3c9a1df872b9e185cce92dac6
Jihun Park <jxxparker@gmail.com>
2022-09-17 12:03:45 -0500

    push

--- 01/14/script.js
+++ 01/14/script.js
@@ -1,29 +1,15 @@
-// let javascriptIsFun = true;
-// console.log(javascriptIsFun);
 
-// console.log(typeof javascriptIsFun);
-// javascriptIsFun = 'Yes';
-// console.log(typeof javascriptIsFun);
+const now = 2037
+const ageJihun = now - 1991;
+const ageJames = now - 1992;
 
-// let year;
-// console.log(year);
-// console.log(typeof year);
+console.log(ageJihun, ageJames)
+console.log(ageJihun * 2, ageJames/2)
 
-// year = 1991;
-// console.log(typeof year);
-// console.log(typeof null);
+const firstName = 'Jihun';
+const lastName = 'Park';
+console.log(firstName + " " + lastName);
 
-// let is used if you wanna change value
-let age = 30;
-age = 31;
-
-//const can't be changed
-const birthYear = 1991;
-console.log(birthYear);
-
-var job = 'programmer';
-job = 'teacher';
-console.log(job);
-
-lastName = 'Park';
-console.log(lastName);
+let x = 10 * 5;
+x += 10
+console.log(x);

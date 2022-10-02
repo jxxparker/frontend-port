6ecb0c84c34b916d59fee984657b12d8536f1aeb
Jihun Park <jxxparker@gmail.com>
2022-10-02 10:24:09 -0500

    push

--- 03/32/script.js
+++ 03/32/script.js
@@ -2,3 +2,10 @@
 
 let hasDriversLicense = false;
 const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;

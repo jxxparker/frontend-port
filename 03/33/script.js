9064af09877eab3bb4e5d562c7ad66f93da77560
Jihun Park <jxxparker@gmail.com>
2022-10-10 13:56:22 -0500

    push

--- 03/33/script.js
+++ 03/33/script.js
@@ -1,6 +1,18 @@
 function logger() {
-    console.log('My name is Jonas');
+  console.log('My name is Jonas');
 }
 
 //calling / running / invoking
 logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);

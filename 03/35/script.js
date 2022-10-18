3fbb45ae9c24225fa77a14067fca4bfc014b7349
Jihun Park <jxxparker@gmail.com>
2022-10-18 00:27:22 -0500

    push

--- 03/35/script.js
+++ 03/35/script.js
@@ -1,11 +1,10 @@
+
 // Function expression
 const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
 }
 
-// Arrow function
+// Arrow Function
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
-console.log(age3);
-
-const yearsUntilRetirement = birthYear => 
+console.log("calcAge3 is " + age3)

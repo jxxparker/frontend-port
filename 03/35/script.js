cca8d77c9695c2832311e7fc76b9a819f95b1173
Jihun Park <jxxparker@gmail.com>
2022-10-19 21:27:01 -0500

    push

--- 03/35/script.js
+++ 03/35/script.js
@@ -8,3 +8,11 @@ const calcAge2 = function (birthYear) {
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
 console.log("calcAge3 is " + age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1992, 'Jihun'));

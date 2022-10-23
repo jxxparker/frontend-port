54748e9f1273203b1f0880834d288d9eb7eaac54
Jihun Park <jxxparker@gmail.com>
2022-10-23 14:10:22 -0500

    completed array

--- 03/39/script.js
+++ 03/39/script.js
@@ -6,6 +6,15 @@ const friends = ['Michael', 'Jimmy', 'Kobe'];
 
 const years = new Array(1991, 1984, 2020);
 
-
 console.log(years);
 console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Mike';
+// friends = ['JI', 'GHON'];  // you can't do this
+console.log(friends);
+
+const jihun = ['James', 'gHoney', 2022 - 1992, friends];
+console.log(jihun);
+console.log(jihun.length);

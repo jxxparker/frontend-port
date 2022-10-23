30647d53a5afd6eed78c2fcc35a55c897db7e90a
Jihun Park <jxxparker@gmail.com>
2022-10-23 13:49:10 -0500

    pushing arrays

--- 03/39/script.js
+++ 03/39/script.js
@@ -2,4 +2,10 @@
 const friend1 = 'Jihun';
 const friend2 = 'James';
 const friend3 = 'Jordan';
+const friends = ['Michael', 'Jimmy', 'Kobe'];
 
+const years = new Array(1991, 1984, 2020);
+
+
+console.log(years);
+console.log(friends[0]);

b54310adbd403781df06a84eded9a1f0c6354684
Jihun Park <jxxparker@gmail.com>
2022-10-24 14:17:19 -0500

    push

---
+++ 03/44/script.js
@@ -0,0 +1,18 @@
+const jihun = {
+  first: 'Jihun',
+  last: 'Park',
+  age: 2022 - 1992,
+  job: 'Programmer',
+  friends: ['Jordan', 'Mike', 'Shaq'],
+  hasDrivers: true,
+
+  calcAge: function (birthYear) {
+    return 2022 - birthYear;
+  }
+};
+
+const calcAge = function(birthYear) {
+    return 2022 - birthYear;
+}
+
+

0b9cccc4dbe21a55805ec437e90e427b06b0164b
Jihun Park <jxxparker@gmail.com>
2022-10-24 17:31:07 -0500

    push

--- 03/44/script.js
+++ 03/44/script.js
@@ -1,18 +1,47 @@
 const jihun = {
-  first: 'Jihun',
-  last: 'Park',
-  age: 2022 - 1992,
-  job: 'Programmer',
-  friends: ['Jordan', 'Mike', 'Shaq'],
-  hasDrivers: true,
+    firstName: 'Jihun',
+    lastName: 'Park',
+    birthYear: 1992,
+    job: 'Programmer',
+    friends: ['Jordan', 'Mike', 'Shaq'],
+    hasDrivers: true,
 
-  calcAge: function (birthYear) {
-    return 2022 - birthYear;
-  }
+    //   calcAge: function (birthYear) {
+    //     return 2022 - birthYear;
+    //   }
+    //   calcAge: function () {
+    //     // console.log(this);
+    //     return 2022 - this.birthYear;
+    //   }
+    calcAge: function() {
+        this.age = 2022 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function() {
+        
+    }
 };
 
-const calcAge = function(birthYear) {
-    return 2022 - birthYear;
-}
+console.log(jihun.calcAge());
+console.log(`${jihun.age} is my age`);
+
+// console.log(jihun['calcAge'](1991));
+
+
+// Challenge
+// Jihjun is 46 years old teacher. 
+
+
+
+
+
+
+
+
+
+
+
+
 
 

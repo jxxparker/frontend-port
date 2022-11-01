85058f0786d1f18c0fc45f8b969bd187256f1e7f
Jihun Park <jxxparker@gmail.com>
2022-10-31 21:27:59 -0500

    completed button proj

--- 07/81/script.js
+++ 07/81/script.js
@@ -4,6 +4,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -21,5 +22,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-
+document.addEventListener
+('keydown', function(e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+  }
+});
 

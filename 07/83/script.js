ae21e1567609f9e97802f204168b8643ba5771af
Jihun Park <jxxparker@gmail.com>
2022-10-31 23:09:12 -0500

    pushing toggle

--- 07/83/script.js
+++ 07/83/script.js
@@ -19,22 +19,21 @@ diceEl.classList.add('hidden');
 let currentScore = 0;
 
 // Rolling dice functionality
-btnRoll.addEventListener
-('click', function() {
-    // 1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
 
-    // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
 
-    // 3. Check for rolled 1: if true, switch to next player
-    if (dice !== 1) {
-        // Add dice to current score
-      currentScore += dice;
-      current0El.textContent = currentScore;
-    } else {
-        // switch to next player
-    }
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
 });

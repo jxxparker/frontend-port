65097bd73327faab0faf7b2a67b277670665d4be
Jihun Park <jxxparker@gmail.com>
2022-11-03 03:01:21 -0500

    re adding toggle fxn

--- 07/84/script.js
+++ 07/84/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// Selecting Elements
+// Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
+const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -18,13 +18,13 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+const scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
 
-// Rolling dice functionality
+// Rolling Dice Function
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
+  // 1. Generating a random dice roll.
   const dice = Math.trunc(Math.random() * 6) + 1;
   console.log(dice);
 
@@ -38,10 +38,10 @@ btnRoll.addEventListener('click', function () {
     currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
-    // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    // Switch to next Player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    activePlayer = activePlayer === 0 ? 1: 0;
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
   }

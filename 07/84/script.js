469a8fdada339a46dc6c91e5bac2cdd75b0115e6
Jihun Park <jxxparker@gmail.com>
2022-11-03 04:25:18 -0500

    pushing complete

--- 07/84/script.js
+++ 07/84/script.js
@@ -18,7 +18,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0,0];
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -36,12 +36,13 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next Player
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1: 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
   }

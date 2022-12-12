45ddab0bb9bacbb4088acad2613d07767a92f053
Jihun Park <jxxparker@gmail.com>
2022-12-11 18:55:58 -0600

    pushing 129

--- 10/129/script.js
+++ 10/129/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const flight = 'JH123';
+const jihun = {
+    name: 'Jihun Park',
+    passport: 123456789,
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'JH987';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 123456789) {
+        alert('Check In')
+    } else {
+        alert('Wrong passport!')
+    }
+}
+
+// checkIn(flight, jihun);
+// console.log(flight);
+// console.log(jihun);
+
+// is the same as doing ... 
+// const flightNum = flight;
+// const passenger = jihun;
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 10000000);
+}
+
+newPassport(jihun); 
+checkIn(flight, jihun);
+

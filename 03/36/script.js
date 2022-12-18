56787d6ae74daca5544351910578ddda34d5bcf4
Jihun Park <jxxparker@gmail.com>
2022-12-18 03:05:51 -0600

    push

--- 03/36/script.js
+++ 03/36/script.js
@@ -1,14 +1,14 @@
+'use strict';
 
 function cutFruitPieces(fruit) {
     return fruit * 4;
 }
 
-
 function fruitProcessor(apples, oranges) {
     const applePieces = cutFruitPieces(apples);
     const orangePieces = cutFruitPieces(oranges);
 
-    const juice = `juice with ${applePieces} piece of apples and ${orangePieces} pieces of oranges.`;
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
     return juice;
 }
 

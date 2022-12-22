07746ede33bf60d1382a2cca0d5f750dcf2ffede
Jihun Park <jxxparker@gmail.com>
2022-12-21 22:37:24 -0600

    push

--- 03/37/script.js
+++ 03/37/script.js
@@ -1,21 +1,27 @@
+'use strict';
 
+// const yearsRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
 
-const calcAge = function(birthYeah) {
-    return 2022 - birthYeah;
+const calcAge = function (birthYear) {
+  return 2022 - birthYear;
 }
 
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = calcAge(birthYear);
-    const retirement = 65 - age;
-    // return retirement;
-    if (retirement > 0) {
-        return `${firstName} retires in ${retirement} years`;
-    } else {
-        return `${firstName} retired ${retirement * -1} years ago`;
-    }
-    
+const yearsRetirement = function (birthYear, firstName) {
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return 'errors';
+  }
 }
 
-console.log(yearsUntilRetirement(1992, 'James'));
-console.log(yearsUntilRetirement(2001, 'Giannis'));
-console.log(yearsUntilRetirement(1950, 'SteveJobs'));
+console.log(yearsRetirement(1992, 'Jihun'));
+console.log(yearsRetirement(2002, 'Jihunypark'));

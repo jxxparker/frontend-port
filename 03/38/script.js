bcd09bdc7c2c6eb12f0ef29d9d5051366faed150
Jihun Park <jxxparker@gmail.com>
2022-12-22 20:30:30 -0600

    pushing arrays

--- 03/38/script.js
+++ 03/38/script.js
@@ -1,20 +1,22 @@
 
 const calcAverage = (a, b, c) => (a + b + c) / 3;
-// console.log(calcAverage(3, 4, 5));
+console.log(calcAverage(3, 4, 5));
+
 
 // Test 1
-const scoreDolphins = calcAverage(44, 33, 22);
-const scoreKoalas = calcAverage(1, 1, 44);
-console.log(scoreDolphins, scoreKoalas);
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
 
-const checkWinner = function(avgDolphins, avgKoalas) {
-    if (avgDolphins >= 2 * avgKoalas) {
-        console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
-    } else if (avgKoalas >= 2 * avgDolphins) {
-        console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+
+// Create fxn checkWinner
+const checkWinner = function(a, b) {
+    if (a > b) {
+        console.log(`${a} wins`);
+    } else if (b > a) {
+        console.log(`${b} wins`)
     } else {
-        console.log('No one wins');
+        console.log('Draw');
     }
-}
+};
 
 checkWinner(scoreDolphins, scoreKoalas);
--- 03/39/script.js
+++ 03/39/script.js
@@ -1,20 +1,12 @@
+'use strict';
 
 const friend1 = 'Jihun';
 const friend2 = 'James';
-const friend3 = 'Jordan';
-const friends = ['Michael', 'Jimmy', 'Kobe'];
+const friend3 = 'Kobe';
 
-const years = new Array(1991, 1984, 2020);
-
-console.log(years);
-console.log(friends[0]);
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
-
-friends[2] = 'Mike';
-// friends = ['JI', 'GHON'];  // you can't do this
+const friends = ['Jihun', 'James', 'Kobe'];
 console.log(friends);
 
-const jihun = ['James', 'gHoney', 2022 - 1992, friends];
-console.log(jihun);
-console.log(jihun.length);
+const years = new Array(1991, 1994, 2000, 2020);
+
+console.log(friends[1]);

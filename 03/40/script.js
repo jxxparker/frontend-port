048143b533333251b1f202eb248078d58409ad79
Jihun Park <jxxparker@gmail.com>
2022-12-22 21:49:49 -0600

    basic array ops

--- 03/40/script.js
+++ 03/40/script.js
@@ -2,25 +2,22 @@
 
 // Add Elements
 const friends = ['Jihun', 'Jordan', 'Mike'];
-const newLength = friends.push('Kobe');
+const newFriends = friends.push('Jay');
 console.log(friends);
-console.log(newLength);
+console.log(newFriends);
 
-friends.unshift('John');
+friends.unshift('John'); // Puts it on first
 console.log(friends);
 
 
-// Remove Elements
-friends.pop();
-friends.pop();
-const popped = friends.pop();
+// Remove elements
+const popped = friends.pop(); // Last
 console.log(popped);
 console.log(friends);
 
-// friends.shift();
+friends.shift(); //removes unshift
 console.log(friends);
-console.log(friends.indexOf('Jihun'));
-console.log(friends.includes('Bob'));
-console.log(friends.includes('John'));
-friends.push(23);
-console.log(friends);
+
+console.log(friends.indexOf('Jihun')); // shows where they are placed
+
+console.log(friends.includes('Jordan'));

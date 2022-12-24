7e0173b42fe8da8674bcb486e06c4f26a6c78119
Jihun Park <jxxparker@gmail.com>
2022-12-24 17:48:03 -0600

    p[ushing #3

--- 03/45/script.js
+++ 03/45/script.js
@@ -1,35 +1,35 @@
 // Coding Challenge # 3
 
 const mark = {
-    fullName: 'Mark Cuban',
-    mass: 10,
-    height: 1.69,
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
 
-    calcBmi: function() {
-        this.bmi = this.mass / this.height ** 2;
-        return this.bmi;
-    }
+  calcBMI: function() {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
 };
 
-const jimmy = {
-  fullName: 'Jimmy Butler',
-  mass: 80,
-  height: 1.25,
+const john = {
+  fullName: 'John Smith',
+  mass: 78,
+  height: 1.69,
 
-  calcBmi: function () {
+  calcBMI: function () {
     this.bmi = this.mass / this.height ** 2;
     return this.bmi;
   },
 };
 
-mark.calcBmi();
-jimmy.calcBmi();
-console.log(mark.bmi, jimmy.bmi);
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.calcBMI(), john.calcBMI());
 
-if (mark.bmi > jimmy.bmi) {
-    console.log(`${mark.fullName}'s BMI ${mark.bmi} is higher.`)
-} else if (jimmy.bmi > mark.bmi) {
-    console.log(`${jimmy.fullName}'s BMI ${jimmy.bmi} is higher.`);
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+} else {
+  console.log('John and Mark have same BMI')
 }
-
-

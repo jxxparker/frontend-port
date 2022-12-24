6410a8a4109f9c3205844b4f07e141571a63c221
Jihun Park <jxxparker@gmail.com>
2022-12-23 21:49:17 -0600

    pushing

--- 03/44/script.js
+++ 03/44/script.js
@@ -1,45 +1,35 @@
-const jihun = {
-    firstName: 'Jihun',
-    lastName: 'Park',
-    birthYear: 1992,
-    job: 'Programmer',
-    friends: ['Jordan', 'Mike', 'Shaq'],
-    hasDrivers: false,
 
-    //   calcAge: function (birthYear) {
-    //     return 2022 - birthYear;
-    //   }
-    //   calcAge: function () {
-    //     // console.log(this);
-    //     return 2022 - this.birthYear;
-    //   }
-    calcAge: function() {
-        this.age = 2022 - this.birthYear;
-        return this.age;
-    },
 
-    getSummary: function() {
-        return `${this.firstName} is a ${this.calcAge()} - years old ${jihun.job}, and he has ${this.hasDrivers ? 'a' : 'no'} drivers license.`
-    }
+const jihun1 = {
+  firstName: 'Jihun',
+  lastName: 'Park',
+  birthYear: 1992,
+  job: 'Programmer',
+  friends: ['Jordan', 'Mike', 'Shaq'],
+  hasDrivers: true,
+
+  // calcAge: function(birthYear) {
+  //     return 2022 - birthYear;
+  // }
+
+  calcAge: function() {
+    console.log(this);
+    return 2022 - this.birthYear; 
+  },
+
+  getSummary: function() {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${jihun.job}, and he has ${this.hasDrivers ? 'a' : 'no'} driver's license`
+  }
 };
 
-console.log(jihun.calcAge());
-console.log(`${jihun.age} is my age`);
+console.log(jihun1.calcAge());
+// console.log(jihun.calcAge(1992));
+// console.log(jihun['calcAge'](1992));
 
-// console.log(jihun['calcAge'](1991));
+// Challenge 
+// Jonas is 46 year old teacher and he has driver's license
 
-
-// Challenge
-// Jihjun is 46 years old teacher. 
-console.log(jihun.getSummary());
-
-
-
-
-
-
-
-
+console.log(jihun1.getSummary());
 
 
 

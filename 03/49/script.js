d7faee1fbc6c286254971b758c0d188f9378bd65
Jihun Park <jxxparker@gmail.com>
2022-12-24 22:30:48 -0600

    pushing coding challenge

--- 03/49/script.js
+++ 03/49/script.js
@@ -1,14 +1,14 @@
 // while loop
 
-// for (let rep = 1; rep <= 10; rep++) {
-//     console.log(`Lifting weights repetition ${rep}`);
-// }
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
 
-// let rep = 1;
-// while (rep <= 10) {
-//     console.log(`Lifting weights repetition ${rep}`);
-//     rep++;
-// }
+let rep = 1;
+while (rep <= 10) {
+    console.log(`Lifting weights repetition ${rep}`);
+    rep++;
+}
 
 let dice = Math.trunc(Math.random() * 6)
 

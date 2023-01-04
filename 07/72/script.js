fa309b060a36ae48d994ef9caa878ffb75ff2e9d
Jihun Park <jxxparker@gmail.com>
2023-01-03 21:19:22 -0600

    push 72

--- 07/72/script.js
+++ 07/72/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number';
+document.querySelector('.message').textContent = 'Correct number ;)'
 
 document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 29;
+document.querySelector('.score').textContent = 15;
+
+
 console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 69;
+document.querySelector('.guess').value = 23;
+

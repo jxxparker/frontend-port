48d71b54ab91942f2189551f54e96bbf19958b99
Jihun Park <jxxparker@gmail.com>
2023-01-04 22:06:19 -0600

    partial 74

--- 07/74/script.js
+++ 07/74/script.js
@@ -1,40 +1,18 @@
 'use strict';
 
-const secretNum = Math.trunc(Math.random() *20) + 1;
-let score = 10;
-document.querySelector('.number').textContent = secretNum;
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // random number from 0 - 19
+document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
 
-document.querySelector('.check').addEventListener
-('click', function() {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
-    if (!guess) {
-        document.querySelector('.message').textContent = 'No Number!';
-    } else if (guess === secretNum) {
-        document.querySelector('.message').textContent = 'Correct  Number!'
-
-    } else if (guess > secretNum) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too High!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game!'
-        }
-
-    } else if (guess < secretNum) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too Low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
-        }   
-    }
+  if (!guess) {
+    document.querySelector('.message').textContent = '👻 No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '👍🏻 Correct Number';
+  } else if (guess > secretNumber) {
+    
+  }
 });
-
-
-
-
-

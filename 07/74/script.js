5fa39eedfde62e36ee0301aac8c547c50ba0ee02
Jihun Park <jxxparker@gmail.com>
2023-01-05 21:16:40 -0600

    changing methods

--- 07/74/script.js
+++ 07/74/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1; // random number from 0 - 19
+let score = 10;
 document.querySelector('.number').textContent = secretNumber;
-console.log(secretNumber);
+
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -13,6 +14,20 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👍🏻 Correct Number';
   } else if (guess > secretNumber) {
-    
-  }
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '☠️ You lost the game';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '☠️ You lost the game';
+    }
+  } 
 });

e32402180f5b607e0fce48536e9ea3eb399f902d
Jihun Park <jxxparker@gmail.com>
2023-01-14 21:35:54 -0600

    pushing 80

--- 07/80/script.js
+++ 07/80/script.js
@@ -4,19 +4,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) 
-  btnsOpenModal[i].addEventListener('click', function() {
+const openModal = function() {
     console.log('Button Clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden'); 
-});
+};
 
-// Close the modal.
-btnCloseModal.addEventListener('click', function() {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-})
-;
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) 
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 

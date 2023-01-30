2ecda24a011cd30cbab8eeffbf0c84a69025ed0e
Jihun Park <jxxparker@gmail.com>
2023-01-29 21:20:48 -0600

    pushing flight ex

--- 10/129/script.js
+++ 10/129/script.js
@@ -4,31 +4,47 @@ const flight = 'JH123';
 const jihun = {
     name: 'Jihun Park',
     passport: 123456789,
-}
+};
 
 const checkIn = function(flightNum, passenger) {
-    flightNum = 'JH987';
+    flightNum = 'ABC999';
     passenger.name = 'Mr. ' + passenger.name;
 
     if (passenger.passport === 123456789) {
-        alert('Check In')
+        alert('Checked In')
     } else {
         alert('Wrong passport!')
     }
-}
+};
 
-// checkIn(flight, jihun);
-// console.log(flight);
-// console.log(jihun);
+checkIn(flight, jihun);
+console.log(flight);
+console.log(jihun);
 
-// is the same as doing ... 
+// Is the same as doing ...
 // const flightNum = flight;
 // const passenger = jihun;
 
 const newPassport = function(person) {
-    person.passport = Math.trunc(Math.random() * 10000000);
-}
+    person.passport = Math.trunc(Math.random() * 1);
+};
 
-newPassport(jihun); 
+newPassport(jihun);
+console.log(flight, jihun);
 checkIn(flight, jihun);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

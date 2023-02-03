bda3687dd5b73d199bda0bc6dac8a2d13185aa8c
Jihun Park <jxxparker@gmail.com>
2023-02-02 22:18:58 -0600

    push

--- 10/134/script.js
+++ 10/134/script.js
@@ -29,10 +29,49 @@ const swiss = {
 };
 
 // Call Method
-lufthansa.book(239, 'Jihun Park');
-book.call(eurowings, 23, 'Sarah wills');
-book.call(lufthansa, 239, 'Jimmy Buttler');
+// lufthansa.book(239, 'Jihun Park');
+// book.call(eurowings, 23, 'Sarah wills');
+// book.call(lufthansa, 239, 'Jimmy Buttler');
 
 // Apply method
-const flightData = [583, 'Geroge Cooper'];
-book.apply(swiss, flightData);
+// const flightData = [583, 'Geroge Cooper'];
+// book.apply(swiss, flightData);
+
+// Bind Method
+const bookEw = book.bind(eurowings);
+const bookLh = book.bind(lufthansa);
+const bookSw = book.bind(swiss);
+// bookEw(23, 'Mike Jordan');
+
+const bookEw23 = book.bind(eurowings, 34);
+// bookEw23('Jihun Park');
+// bookEw23('James Park');
+
+
+// With event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function() {
+    console.log(this);
+
+    this.planes++
+    console.log(this.planes);
+};
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVat = addTax.bind(null, 0.23);
+// addVat = value => value + value * 0.23
+console.log(addVat(100));
+
+const addTaxRate = function(rate) {
+    return function (value) {
+        return value + value * rate;
+    };
+};
+
+const addVat2 = addTaxRate(0.23);
+console.log(addVat2(100));

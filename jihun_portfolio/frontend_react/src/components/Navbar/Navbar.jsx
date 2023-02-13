33ee56e5b024a405193144ccde479992ad24e472
Jihun Park <jxxparker@gmail.com>
2023-02-12 22:04:19 -0600

    pushing 37:35

--- jihun_portfolio/frontend_react/src/components/Navbar/Navbar.jsx
+++ jihun_portfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
+import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
   return (
     <nav>
         <div>
-            <img src={image.logo} alt="logo"/>
+            <img src={images.logo} alt="logo"/>
         </div>
+        <ul>
+          {['home', 'about', 'contact', 'work', 'skills'].map((item) => ( 
+            <li key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+         ))}
+        </ul>
     </nav>
   )
 };

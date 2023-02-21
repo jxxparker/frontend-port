849162f7c3e2e0e19582eb7eb849b7b76d1d9a78
Jihun Park <jxxparker@gmail.com>
2023-02-21 11:17:22 +0900

    pushing 10522

--- jihun_portfolio/frontend_react/src/components/Navbar/Navbar.jsx
+++ jihun_portfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -29,14 +29,15 @@ const Navbar = () => {
             whileInView={{ x: [300, 0] }}
             transition={{ duration: 0.85, ease: 'easeOut' }}>
             <HiX onClick={() => setToggle(false)} />
-
-            {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
-              <li key={item}>
-                <a href={`#${item}`} onClick={() => setToggle(false)}>
-                  {item} 
-                </a>
-              </li>
-            ))}
+            <ul>
+              {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </motion.div>
         )}
       </div>
--- jihun_portfolio/frontend_react/src/container/Header/Header.jsx
+++ jihun_portfolio/frontend_react/src/container/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion }
 
 import './Header.scss';
 const Header = () => {

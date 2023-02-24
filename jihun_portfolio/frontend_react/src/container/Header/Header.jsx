fff1d14939b7d4550fad13e252d3dbed2b87ab77
Jihun Park <jxxparker@gmail.com>
2023-02-23 19:56:41 -0600

    pushing 11252

--- jihun_portfolio/frontend_react/src/container/Header/Header.jsx
+++ jihun_portfolio/frontend_react/src/container/Header/Header.jsx
@@ -28,13 +28,22 @@ const Header = () => {
         </div>
       </motion.div>
 
-      <motion.div>
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.7, delayChildren: 0.5 }}
+        className="app__header-img">
+        <img src={images.profile} alt="profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle">
 
+          </motion.img>
       </motion.div>
 
-      <motion.div>
-
-      </motion.div>
+      <motion.div></motion.div>
     </div>
   );
 };

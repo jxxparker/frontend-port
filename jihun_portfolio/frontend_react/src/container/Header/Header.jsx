1edfd7e7b6780e8c8f7cf893e5a4c5ed23e55d90
Jihun Park <jxxparker@gmail.com>
2023-02-25 00:23:03 -0600

    pushing 12544

--- jihun_portfolio/frontend_react/src/container/Header/Header.jsx
+++ jihun_portfolio/frontend_react/src/container/Header/Header.jsx
@@ -17,7 +17,7 @@ const scaleVariants = {
 }
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 1 }}
@@ -55,8 +55,12 @@ const Header = () => {
          variant ={scaleVariants}
          whileInView={scaleVariants.whileInView}
          className="app_header-circles"
-
       >
+          {[images.flutter, images.redux, images.sass].map((circle, index) => (
+            <div className="circle-cmp app__flex" key={`circle-${index}`}>
+              <img src={circle} alt="circle"/>
+            </div>
+          ))}
       </motion.div>
     </div>
   );

81bdc17c64b700a2e8222d8d41747db6c899047a
Jihun Park <jxxparker@gmail.com>
2023-03-09 22:14:26 -0600

    pushing 20918

--- jihun_portfolio/frontend_react/src/client.js
+++ jihun_portfolio/frontend_react/src/client.js
@@ -2,12 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: 'j3q3wak1',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token:
-    'sk4gpYlU7ayPIav2p2FtnXuAdLj25jR1tMjoKtgSZTwarCzlwgzCKEvPnGsyny3l8AkFAnIrc5ueSmqL8I16uAiAING9JGzcejhrkgpz2XFGmxZdiG57siOZVSsUBgOSGspNjs5qJgAJYG7KiLToJv5Jn6EMPLS4fQDFlIc33E4v3N5laGpF',
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
--- jihun_portfolio/frontend_react/src/container/About/About.jsx
+++ jihun_portfolio/frontend_react/src/container/About/About.jsx
@@ -1,30 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { images} from '../../constants';
 import './About.scss';
+import { urlFor, client } from '../../client';
+import { AppWrap } from '../../wrapper';
 
-const abouts = [
-  {
-    title: 'Web Develoment',
-    description: 'I am good web develoer.',
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Web Design',
-    description: 'I am good web develoer.',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'UI/UX',
-    description: 'I am good web develoer.',
-    imgUrl: images.about03,
-  },
-];
+// const abouts = [
+//   {
+//     title: 'Web Develoment',
+//     description: 'I am good web develoer.',
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: 'Web Design',
+//     description: 'I am good web develoer.',
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: 'UI/UX',
+//     description: 'I am good web develoer.',
+//     imgUrl: images.about03,
+//   },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
-      <h2 className="head-text">I Know that <span> Good Design </span> <br /> means <span>Good Business</span> </h2>
+      <h2 className="head-text">
+        I Know that <span> Good Design </span> <br /> means{' '}
+        <span>Good Business</span>{' '}
+      </h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
@@ -34,8 +48,7 @@ const About = () => {
             transition={{ duration: 0.5, type: 'tween' }}
             className="app__profile-item"
             key={about.title + index}>
-
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
@@ -49,4 +62,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
